8bbf38cefda17dc7d6e58e46dca76c4ac92d2357
Gabriella <128740583+morgadogs@users.noreply.github.com>
2023-06-01 16:16:06 -0300

    Adjust formatting

--- src/poly.py
+++ src/poly.py
@@ -1,16 +1,15 @@
 import math
 
-"""
-The initialization receives a dict where the keys are non negative degrees and the values are the coefficients.
-"""
 class Polynomial:
-
+    """
+    The initialization receives a dict where the keys are non negative degrees and the values are the coefficients.
+    """
     def __init__ (self, coef_at_degree):
-
         self.coef_at_degree = coef_at_degree
-"""
-Evaluates the value of the polynomial at a given x.
-"""
+
+    """
+    Evaluates the value of the polynomial at a given x.
+    """
     def eval (self, x):
         value_at_x = 0
         for degree in self.coef_at_degree:
